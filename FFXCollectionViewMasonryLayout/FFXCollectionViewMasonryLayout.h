06d5ac8c06dbb3e4ba9784a962aea2d32991e5c6
Sebastian Boldt <sebastian.boldt@empora.com>
2015-02-06 12:56:13 +0100

    Initial commit 2

---
+++ FFXCollectionViewMasonryLayout/FFXCollectionViewMasonryLayout.h
@@ -0,0 +1,27 @@
+//
+//  FFXCollectionViewMasonryLayout.h
+//  FFXCollectionViewMasonryLayout
+//
+//  Created by Sebastian Boldt on 06.02.15.
+//  Copyright (c) 2015 Empora. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+@class FFXCollectionViewMasonryLayout;
+// This Protocol defines all the Methods the Delegate should implement
+@protocol UICollectionViewDelegateFFXMasonryLayout<NSObject>
+
+// This function returns the size of an item at a specific indexPath
+@required
+- (CGSize) collectionView:(UICollectionView*) collectionView
+                   layout:(FFXCollectionViewMasonryLayout*) layout
+   sizeForItemAtIndexPath:(NSIndexPath*) indexPath;
+@end
+
+@interface FFXCollectionViewMasonryLayout : UICollectionViewLayout
+@property(nonatomic,assign)NSInteger numberOfColums;    // Number of Columns that should be displayed
+@property(nonatomic,assign)NSInteger interItemSpacing;  // Spacing between items
+
+@property(nonatomic,weak) IBOutlet id<UICollectionViewDelegateFFXMasonryLayout> delegate; // delegate that returns size for each Item
+@end
---
+++ FFXCollectionViewMasonryLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CollectionViewCustomLayout
+//
+//  Created by Sebastian Boldt on 03.02.15.
+//  Copyright (c) 2015 sebastianboldt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
